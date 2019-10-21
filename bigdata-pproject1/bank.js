e5b95831d7f1c56a195e28f4b1b741c5cfc18a91
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2019-10-21 18:03:46 +0300

    Added mock data.

--- bigdata-pproject1/bank.js
+++ bigdata-pproject1/bank.js
@@ -12,7 +12,7 @@ var dbw = {
         document.additionalName = '';
      }
     
-     document.departmentId = db.departments.find({name: departmentName})[0]._id;
+     document.departmentId = db.departments.find({name: document.departmentName})[0]._id;
 
      db.employees.insert(document);
     },
@@ -35,13 +35,13 @@ var dbw = {
         }
 
         db.clients.insert(document);
-        var clientId = db.clients.find({}).sort({_id:-1}).limit(1);
-        this.insertAccountForClient(clientId);
+        var client = db.clients.find({}).sort({_id:-1}).limit(1)[0];
+        this.insertAccountForClient(client._id);
 
     },
 
     //Сметки
-    insertAccountForClient: function(clientIdToInsert, currencyToInsert = 'BGN'){
+    insertAccountForClient: function(clientIdToInsert, currencyToInsert = 'BGN', balanceToAdd = 1){
         var accountName = generateAccountName();
         while(!validator.accountNameIsValid(accountName)){
             accountName = generateAccountName();
@@ -49,9 +49,10 @@ var dbw = {
         var account = {
             name: accountName,
             clientId: clientIdToInsert,
-            currency: currencyToInsert
+            currency: currencyToInsert,
+            balance: balanceToAdd
         };
-
+        
         if(!validator.accountIsValid(account)){
             print('Please enter valid account data');
             return;
@@ -78,12 +79,12 @@ var validator = {
         }
 
         if(!document.departmentName || document.departmentName ==''){
-            print('Please enter valid departmentName!');
+            print('Please enter valid department Name!');
             isValid =  false;
             return isValid;
         }
 
-        var department = db.departments.find({name: departmentName})[0]
+        var department = db.departments.find({name: document.departmentName})[0]
 
         if(!department){
             print('Please enter valid departmentName!');
@@ -91,7 +92,7 @@ var validator = {
         }
 
         if(document.superiorId){
-            var superior = db.employees.find({_id: superiorId})[0];
+            var superior = db.employees.find({_id: document.superiorId})[0];
             if(!superior){
                 print('Please enter valid superiorId!');
                 isValid =  false;
@@ -147,8 +148,14 @@ var validator = {
     accountIsValid: function(document){
         var isValid = true;
 
-        if(!document.userId){
+        if(!document.clientId){
             print('Please enter valid client id!');
+            isValid = false;
+            return isValid;
+        }
+
+        if(!document.balance && document.balance!==0){
+            print('Please enter valid balance value!');
             isValid = false;
             return isValid;
         }
@@ -158,7 +165,7 @@ var validator = {
             isValid = false;
         }
 
-        var user = db.clients.find({_id: document.userId})[0];
+        var user = db.clients.find({_id: document.clientId})[0];
         if(!user){
             print('Please enter valid client id!');
             isValid = false;
@@ -189,3 +196,242 @@ var generateAccountName = function(){
     }
     return accountId;
 }
+
+//#region Валидни Данни за отдели
+//Валидни Данни за отдели
+dbw.insertDepartment({
+    name: 'Corporate'
+})
+
+dbw.insertDepartment({
+    name: 'Private'
+})
+
+dbw.insertDepartment({
+    name: 'HR'
+})
+
+dbw.insertDepartment({
+    name: 'Security'
+})
+
+//Невалидни Данни за отдели
+dbw.insertDepartment({
+    name: ''
+})
+
+dbw.insertDepartment({
+})
+//#endregion
+
+//#region Данни за служители
+//Данни за служители
+dbw.insertEmployee({
+    firstName: 'Ivan',
+    lastName: 'Ivanov',
+    address:'str. Duruburu 1',
+    email: 'ii@brb.bg',
+    phoneNumber: '080908u9',
+    position: 'banker',
+    departmentName: 'Corporate'
+})
+
+dbw.insertEmployee({
+    firstName: 'Dragan',
+    lastName: 'Ivanov',
+    address:'str. Duruburu 1',
+    email: 'di@brb.bg',
+    phoneNumber: '08090849',
+    position: 'banker',
+    departmentName: 'Private'
+})
+
+dbw.insertEmployee({
+    firstName: 'Ivan',
+    lastName: 'Draganov',
+    address:'str. Duruburu 3',
+    email: 'id@brb.bg',
+    phoneNumber: '08090859',
+    position: 'guard',
+    departmentName: 'Security'
+})
+
+dbw.insertEmployee({
+    firstName: 'Stoian',
+    lastName: 'Ivanov',
+    address:'str. Duruburu 5',
+    email: 'si@brb.bg',
+    phoneNumber: '08090149',
+    position: 'guard',
+    departmentName: 'Private',
+
+
+})
+
+
+dbw.insertEmployee({
+    firstName: 'Uvuvle',
+    lastName: 'Vuvle',
+    additionalName:"VuvlevuvuVleVuvuvVlebuVuvlav",
+    address:'str. Duruburu 10',
+    email: 'vvv@brb.bg',
+    phoneNumber: '18090149',
+    position: 'guard',
+    departmentName: 'HR',
+
+
+})
+
+dbw.insertEmployee({
+    firstName: 'Rajit',
+    lastName: 'Budala',
+    additionalName:"Tuzu",
+    address:'str. Duruburu 13',
+    email: 'bigdaddybuge420@brb.bg',
+    phoneNumber: '18090169',
+    position: 'CS',
+    departmentName: 'HR',
+
+
+})
+
+dbw.insertEmployee({
+    firstName: 'Petkan',
+    lastName: 'Ivanov',
+    address:'str. Duruburu 1',
+    email: 'Pi@brb.bg',
+    phoneNumber: '08090879',
+    position: 'banker',
+    departmentName: 'Corporate'
+})
+
+dbw.insertEmployee({
+    firstName: 'Jeliasko',
+    lastName: 'Kamenov',
+    address:'str. Duruburu 69',
+    email: 'testiculartunder@brb.bg',
+    phoneNumber: '08090666',
+    position: 'guard',
+    departmentName: 'Security'
+})
+
+//Не валидни данни за служители
+dbw.insertEmployee({
+    firstName: 'Stoian',
+    lastName: 'Ivanov',
+    address:'str. Duruburu 5',
+    email: 'si@brb.bg',
+    phoneNumber: '08090149',
+    position: 'guard',
+    departmentName: 'Private',
+
+})
+
+dbw.insertEmployee({
+    firstName: '',
+    lastName: 'Kamenov',
+    address:'str. Duruburu 69',
+    email: 'testiculartunder@brb.bg',
+    phoneNumber: '08090666',
+    position: 'guard',
+    departmentName: 'Security'
+})
+
+dbw.insertEmployee({
+    firstName: 'Jeliasko',
+    address:'str. Duruburu 69',
+    email: 'testiculartunder@brb.bg',
+    phoneNumber: '08090666',
+    position: 'guard',
+    departmentName: 'Security'
+})
+
+dbw.insertEmployee({
+    firstName: 'Jeliasko',
+    lastName: 'Kamenov',
+    email: 'testiculartunder@brb.bg',
+    phoneNumber: '08090666',
+    position: 'guard',
+    departmentName: 'Security'
+})
+
+dbw.insertEmployee({
+    firstName: 'Jeliasko',
+    lastName: 'Kamenov',
+    address:'str. Duruburu 69',
+    phoneNumber: '08090666',
+    position: 'guard',
+    departmentName: 'Security'
+})
+
+dbw.insertEmployee({
+    firstName: 'Jeliasko',
+    lastName: 'Kamenov',
+    address:'str. Duruburu 69',
+    email: 'testiculartunder@brb.bg',
+    position: 'guard',
+    departmentName: 'Security'
+})
+
+dbw.insertEmployee({
+    firstName: 'Jeliasko',
+    lastName: 'Kamenov',
+    address:'str. Duruburu 69',
+    email: 'testiculartunder@brb.bg',
+    phoneNumber: '08090666',
+    departmentName: 'Security'
+})
+
+dbw.insertEmployee({
+    firstName: 'Jeliasko',
+    lastName: 'Kamenov',
+    address:'str. Duruburu 69',
+    email: 'testiculartunder@brb.bg',
+    phoneNumber: '08090666',
+    departmentName: 'Trust and safety'
+})
+//#endregion
+
+//#region Данни за клиенти
+dbw.insertClient({
+    firstName: 'Ivan',
+    lastName: 'Ivanov',
+    address:'str. Duruburu 1',
+    email: 'ii@brb.bg',
+    phoneNumber: '080908u9'
+})
+
+dbw.insertClient({
+    firstName: 'Ivan',
+    lastName: 'Ivanov',
+    address:'str. Duruburu 1',
+    email: 'ii@brb.bg',
+    phoneNumber: '080908u9',
+})
+
+dbw.insertClient({
+    firstName: 'Dragan',
+    lastName: 'Ivanov',
+    address:'str. Duruburu 1',
+    email: 'di@brb.bg',
+    phoneNumber: '08090849'
+})
+
+dbw.insertClient({
+    firstName: 'Ivan',
+    lastName: 'Draganov',
+    address:'str. Duruburu 3',
+    email: 'id@brb.bg',
+    phoneNumber: '08090859',
+})
+
+dbw.insertClient({
+    firstName: 'Stoian',
+    lastName: 'Ivanov',
+    address:'str. Duruburu 5',
+    email: 'si@brb.bg',
+    phoneNumber: '08090149',
+})
+
+
+//#endregion
